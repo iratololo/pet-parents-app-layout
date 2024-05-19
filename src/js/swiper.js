bf77c83945089610e53450cce504765e86bf4780
iratololo <iratololo1991@gmail.com>
2024-05-19 18:04:16 +0300

    settings

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
+import {Pagination, Autoplay, EffectFade } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
